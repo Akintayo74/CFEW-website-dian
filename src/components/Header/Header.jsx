d2642a180d231a9f8cda8cd108a6d2f84bf9132b
Akintayo74 <akinnibosunakintayo74@gmail.com>
2025-09-17 14:52:03 +0100

    Styling Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,60 @@
 import React from 'react';
-import Section from '@/components/Section';
 import logo from '@/assets/CFEW-Logo.png';
-import { Menu } from 'lucide-react';
+import { Menu, X } from 'lucide-react';
+import { href } from 'react-router-dom';
 
 function Header() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = React.useState(true);
+
+  function toggleMobileMenu() {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  }
+
+  const navLinks = [
+    {
+      href: '/',
+      label: 'Home',
+    },
+    {
+      href: '/about',
+      label: 'About',
+    },
+    {
+      href: '/programs',
+      label: 'Programs',
+    },
+    {
+      href: '/resources',
+      label: 'Resources'
+    }
+  ]
+
   return (
-    <div className='bg-yellow-300'>
-      <Section />
+    <header className='bg-yellow-300 flex justify-between items-center'>
       <img src={logo} />
-      <Menu />
-    </div>
+
+      <div>
+        {/* Mobile First design */}
+        <button onClick={toggleMobileMenu} className='md:hidden' aria-label="Toggle mobile menu">
+          {isMobileMenuOpen ? <X /> : <Menu />}
+        </button>
+
+        {isMobileMenuOpen && (
+          <nav className='flex flex-col'>
+            {navLinks.map((link) => (
+              <a
+                key={link.href}
+                href={link.href}
+                onClick={() => setIsMobileMenuOpen(false)}
+              >
+                { link.label }
+              </a>
+            ))}
+          </nav>
+        )}
+      </div>
+
+    </header>
   );
 }
 
