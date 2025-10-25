c7f8253d35d8ccd15b1ac40de52badb8be025d25
Akintayo74 <akinnibosunakintayo74@gmail.com>
2025-10-25 15:03:25 +0100

    Add zoom in animation on hover to 'OurThematicAreas' section

--- src/components/ThematicAreas/ThematicAreas.jsx
+++ src/components/ThematicAreas/ThematicAreas.jsx
@@ -13,31 +13,31 @@ function ThematicAreas() {
         </p>
       </div>
 
-      <div className="grid grid-cols-2 gap-0 lg:grid-cols-4 ">
-        <a href="/about#environment" className="relative group">
-          <img src="./thematic-environment.png" className="h-full w-full" />
-          <div className="absolute inset-0 bg-black/40 opacity-0 transition-opacity duration-70  group-hover:opacity-100"></div>
+      <div className="grid grid-cols-2 gap-0 lg:grid-cols-4">
+        <a href="/about#environment" className="relative group overflow-hidden block">
+          <img src="./thematic-environment.png" className="h-full w-full block object-cover transition-transform duration-500 group-hover:scale-120" />
+          <div className="absolute inset-0 bg-black/40 opacity-0 transition-opacity duration-500  group-hover:opacity-100"></div>
           <p className="text-cfew-primary-50 absolute bottom-5 left-5 text-sm font-bold md:text-2xl lg:bottom-10 lg:left-10">
             Environment
           </p>
         </a>
-        <a href="/about#health" className="relative group">
-          <img src="./thematic-health.png" className="h-full w-full" />
-          <div className="absolute inset-0 bg-black/40 opacity-0 transition-opacity duration-70 group-hover:opacity-100"></div>
+        <a href="/about#health" className="relative group overflow-hidden block">
+          <img src="./thematic-health.png" className="h-full w-full block object-cover transition-transform duration-500 group-hover:scale-120" />
+          <div className="absolute inset-0 bg-black/40 opacity-0 transition-opacity duration-500 group-hover:opacity-100"></div>
           <p className="text-cfew-primary-50 absolute bottom-5 left-5 text-sm font-bold md:text-2xl lg:bottom-10 lg:left-10">
             Health
           </p>
         </a>
-        <a href="/about#agriculture" className="relative group">
-          <img src="./thematic-agriculture.png" className="h-full w-full" />
-          <div className="absolute inset-0 bg-black/40 opacity-0 transition-opacity duration-70 group-hover:opacity-100"></div>
+        <a href="/about#agriculture" className="relative group overflow-hidden block">
+          <img src="./thematic-agriculture.png" className="h-full w-full block object-cover transition-transform duration-500 group-hover:scale-120" />
+          <div className="absolute inset-0 bg-black/40 opacity-0 transition-opacity duration-500 group-hover:opacity-100"></div>
           <p className="text-cfew-primary-50 absolute bottom-5 left-5 text-sm font-bold md:text-2xl lg:bottom-10 lg:left-10">
             Agriculture
           </p>
         </a>
-        <a href="/about#education" className="relative group">
-          <img src="./thematic-education.png" className="h-full w-full" />
-          <div className="absolute inset-0 bg-black/40 opacity-0 transition-opacity duration-70 group-hover:opacity-100"></div>
+        <a href="/about#education" className="relative group overflow-hidden block">
+          <img src="./thematic-education.png" className="h-full w-full block object-cover transition-transform duration-500 group-hover:scale-120" />
+          <div className="absolute inset-0 bg-black/40 opacity-0 transition-opacity duration-500 group-hover:opacity-100"></div>
           <p className="text-cfew-primary-50 absolute bottom-5 left-5 text-sm font-bold md:text-2xl lg:bottom-10 lg:left-10">
             Education
           </p>
