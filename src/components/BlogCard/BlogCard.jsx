e7245c03688a26ef05b8b4cfb9759ae85e958db2
Akintayo74 <akinnibosunakintayo74@gmail.com>
2025-11-04 11:14:15 +0100

    Add transition to navlinks; Increase font size of Section Headers

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,16 +1,9 @@
 import Container from "../Container/Container";
 import React from "react";
 
-function BlogCard({ imageSrc, imageAlt, publishDate, title, background = 'bg-cfew-trial' }) {
+function BlogCard({ imageSrc, imageAlt, publishDate, title, background = 'bg-cfew-blog-default' }) {
   return (
     <div className={`${background} flex flex-col w-full}`}>
-      {/* <div className="flex justify-center">
-        <img
-          src={imageSrc}
-          alt={imageAlt}
-          className="w-full h-50 md:h-60 lg:h-80 object-cover"
-        />
-      </div> */}
 
       <div className="w-full aspect-[4/3] md:aspect-[16/9] lg:aspect-[3/2]">
         <img
@@ -20,7 +13,7 @@ function BlogCard({ imageSrc, imageAlt, publishDate, title, background = 'bg-cfe
         />
       </div>
 
-      <div className="flex flex-col text-left gap-4 p-6">
+      <div className="flex flex-col text-left gap-4 p-6 hover:cursor-pointer transition duration-300 hover:bg-cfew-blog-hover">
         <p className="text-sm md:text-base lg:text-lg text-cfew-dark leading-relaxed">{publishDate}</p>
         <div>
         <h6 className="font-bold text-xl lg:text-2xl text-cfew-dark leading-[1.4] [display:-webkit-box] [-webkit-box-orient:vertical] [-webkit-line-clamp:3] overflow-hidden">{title}</h6>
